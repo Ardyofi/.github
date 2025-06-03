6ac42b4ef08d08f1acbde3557b01aa8d6bc837be
Ardyofi <168245373+Ardyofi@users.noreply.github.com>
2025-06-03 10:19:54 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,14 +1,6 @@
-class countcalls:
-    def __init__(self, func):
-        self.func = func
-        self.count = 0
-    def __call__(self, *args, **kwargs):
-        self.count += 1
-        print(f"# of calls: {self.count}")
-        return self.func(*args, **kwargs)
+a = 10
+b = 5
 
-@countcalls
-def foo(x):
-    return x+2
-foo(3)
-foo(4)
+result = a + b
+
+print("The sum of", a, "and", b, "is", result)
